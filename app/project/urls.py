66d05cab0a8f87fe71ada6a6383abfd246adfd25
EnuchSantos <enuchdev@gmail.com>
2023-05-29 18:19:03 -0300

    docker-configs: docker-compose add, dockerIgnore add, Dockerfile add. variaveis de ambiente para docker configuradas, e pasta de arquivos estaticos ddentro do docker

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
@@ -18,3 +20,9 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
